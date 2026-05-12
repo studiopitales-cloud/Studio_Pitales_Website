a4060a48d116a921b8b2c1adf1b39fac8647a0e0
Studio Pitales <studiopitales@gmail.com>
2026-05-12 20:38:30 +0300

    Remove subscription field

--- api/create-lead.js
+++ api/create-lead.js
@@ -8,7 +8,6 @@ export default function handler(req, res) {
   const payload = JSON.stringify({
     clientData: { fullName: name, phone: phone },
     pipeline: { stage: 32374, leadSource: 16928 },
-    subscription: { itemId: null },
   })
 
   const options = {
